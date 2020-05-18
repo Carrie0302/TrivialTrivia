6d2675c2fd91294f3ba225cad97b527ad63d69c3
Laura Larson <larsonlaura@seattleu.edu>
2020-05-18 12:51:17 -0700

    Update QuizDBHelperTest.java
    Import statements and mock statements

--- app/src/test/java/com/avinashdavid/trivialtrivia/data/QuizDBHelperTest.java
+++ app/src/test/java/com/avinashdavid/trivialtrivia/data/QuizDBHelperTest.java
@@ -1,7 +1,38 @@
 package com.avinashdavid.trivialtrivia.data;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import com.avinashdavid.trivialtrivia.data.QuizDBContract;
+import com.avinashdavid.trivialtrivia.questions.IndividualQuestion;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@RunWith(RobolectricTestRunner.class)
 public class QuizDBHelperTest {
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    Context mockContext;
 
 }
